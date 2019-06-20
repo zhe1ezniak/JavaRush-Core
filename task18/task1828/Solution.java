e653ecf232b04106fa66429fade659872065a03e
zhe1ezniak <dimazhel88@gmail.com>
2019-06-20 19:44:13 +0300

    Some changes

--- task18/task1828/Solution.java
+++ task18/task1828/Solution.java
@@ -1,101 +1,76 @@
 package com.javarush.task.task18.task1828;
 
-/* 
+import java.io.*;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/*
 Прайсы 2
 */
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+        if ("-u".equals(args[0])) {
+            String id = padRight(args[1], 8);
+            String newProductName = padRight(args[2], 30);
+            String newPrice = padRight(args[3], 8);
+            String newQuantity = padRight(args[4], 4);
 
-class Solution {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String fileName = bufferedReader.readLine();
-        bufferedReader.close();
-        StringBuilder productName = new StringBuilder();
-
-        ArrayList<String> list = getStringList(fileName);
-        ArrayList<Long> idList = getIdList(fileName);
-        PrintWriter printWriter;
-        int index = idList.indexOf(Long.parseLong(args[1]));
-
-        if (args[0].equals("-u")) {
-            for (int i = 2; i < args.length - 2; i++) {
-                productName.append(args[i]).append(" ");
-            }
-
-            String trueProductName = setSpaces(productName.toString(), 30);
-            String truePrice = setSpaces(args[args.length - 2], 8);
-            String trueQuantity = setSpaces(args[args.length - 1], 4);
-            String id = setSpaces(args[1], 8);
-            String ourString = id + trueProductName + truePrice + trueQuantity;
-
-            if (!idList.contains(Long.parseLong(args[1]))) {
-                printWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName, true)));
-                printWriter.println(ourString);
-            } else {
-                list.set(index, ourString);
-                printWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
-                for (String aString : list) {
-                    printWriter.println(aString);
+            List<String> fileLines = readFileLines(fileName);
+            StringBuilder builder = new StringBuilder();
+            for (String line : fileLines) {
+                if (id.equals(line.substring(0, 8))) {
+                    builder.setLength(0);
+                    builder.append(id).append(newProductName).append(newPrice).append(newQuantity);
+                    break;
                 }
             }
-            printWriter.close();
-        } else if (args[0].equals("-d")) {
-            list.remove(index);
-            printWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
-            for (String aString : list) {
-                printWriter.println(aString);
+            writeFile(fileName, fileLines);
+
+        }
+        else if ("-d".equals(args[0])) {
+            String id = padRight(args[1], 8);
+
+            List<String> fileLines = readFileLines(fileName);
+            Iterator<String> iter = fileLines.iterator();
+            while (iter.hasNext()) {
+                if (id.equals(iter.next().substring(0, 8))) {
+                    iter.remove();
+                    break;
+                }
             }
-            printWriter.close();
+            writeFile(fileName, fileLines);
         }
     }
 
-    private static ArrayList<String> getStringList(String fileName) throws IOException {
-        ArrayList<String> allList = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            allList.add(line);
+    private static List<String> readFileLines(String fileName) throws IOException {
+        List<String> fileLines = new LinkedList<>();
+        BufferedReader fReader = new BufferedReader(new FileReader(fileName));
+        String str;
+        while ((str = fReader.readLine()) != null) {
+            fileLines.add(str);
         }
-        bufferedReader.close();
-        return allList;
+        fReader.close();
+        return fileLines;
     }
 
-    private static ArrayList<Long> getIdList(String fileName) throws IOException {
-        ArrayList<Long> allIds = new ArrayList<>();
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        String line;
-        Long currentId;
-        while ((line = bufferedReader.readLine()) != null) {
-            line = line.substring(0, 8).replaceAll("\\s", "");
-            currentId = Long.parseLong(line);
-            allIds.add(currentId);
+    private static void writeFile(String fileName, List<String> fileLines) throws IOException {
+        BufferedWriter fWriter = new BufferedWriter(new FileWriter(fileName));
+        for (String line : fileLines) {
+            fWriter.write(line);
+            fWriter.newLine();
         }
-        bufferedReader.close();
-        return allIds;
+        fWriter.close();
     }
 
-
-    @SuppressWarnings("Duplicates")
-    private static String setSpaces(String previousName, int count) {
-        String trueName;
-        if (previousName.length() > count) {
-            trueName = previousName.substring(0, count);
+    private static String padRight(String input, int len) {
+        if (input.length() < len) {
+            return String.format("%1$-" + len + "s", input);
         } else {
-            StringBuilder s = new StringBuilder();
-            for (int i = 0; i < (count - previousName.length()); i++) {
-                s.append(" ");
-            }
-            trueName = previousName + s;
+            return input.substring(0, len);
         }
-        return trueName;
     }
 }
--- task19/task1916/Solution.java
+++ task19/task1916/Solution.java
@@ -10,52 +10,65 @@ import java.util.List;
 /* 
 Отслеживаем изменения
 */
-
 public class Solution {
-    public static List<LineItem> lines = new ArrayList<LineItem>();
+    public static List<LineItem> lines = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        try{
-            String file1 = reader.readLine();
-            String file2 = reader.readLine();
-            reader.close();
+        String oldFileName = reader.readLine();
+        String newFileName = reader.readLine();
+        reader.close();
+        List<String> oldFileLines = readFileLines(oldFileName);
+        List<String> newFileLines = readFileLines(newFileName);
 
-            BufferedReader reader1 = new BufferedReader(new FileReader(file1));
-            BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+        boolean iterateByOldFile = true;
 
-            ArrayList<String> list1 = new ArrayList<>();
-            ArrayList<String> list2 = new ArrayList<>();
-
-            while (reader1.readLine() != null){
-                list1.add(reader1.readLine());
-            }
-            reader1.close();
-            while (reader2.readLine() != null){
-                list2.add(reader2.readLine());
-            }
-            reader2.close();
-            for (int i = 0; i < list1.size(); i++){
-               if (list2.contains(list1.get(i))) {
-                   lines.add(new LineItem(Type.SAME, list1.get(i)));
-               }
-               else lines.add(new LineItem(Type.REMOVED, list1.get(i)));
-            }
-            int j = 0;
-            for (int i = 0; i < lines.size(); i++) {
-                if (lines.get(i).equals(list2.get(j))) {
-                    i++;
-                    j++;
+        while (oldFileLines.size() > 0 && newFileLines.size() > 0) {
+            if (iterateByOldFile){
+                if ( oldFileLines.get(0).equals(newFileLines.get(0)) ) {
+                    lines.add(new LineItem(Type.SAME, oldFileLines.get(0)));
+                    oldFileLines.remove(0);
+                    newFileLines.remove(0);
+                } else if ( oldFileLines.get(0).equals(newFileLines.get(1)) ) {
+                    lines.add(new LineItem(Type.ADDED, newFileLines.get(0)));
+                    newFileLines.remove(0);
+                    iterateByOldFile = !iterateByOldFile;
                 } else {
-                    lines.add(i, new LineItem(Type.ADDED, list2.get(j)));
-                    j++;
+                    lines.add(new LineItem(Type.REMOVED, oldFileLines.get(0)));
+                    oldFileLines.remove(0);
+                }
+            } else {
+                if ( newFileLines.get(0).equals(oldFileLines.get(0)) ) {
+                    lines.add(new LineItem(Type.SAME, newFileLines.get(0)));
+                    newFileLines.remove(0);
+                    oldFileLines.remove(0);
+                } else if ( newFileLines.get(0).equals(oldFileLines.get(1)) ) {
+                    lines.add(new LineItem(Type.REMOVED, oldFileLines.get(0)));
+                    oldFileLines.remove(0);
+                    iterateByOldFile = !iterateByOldFile;
+                } else {
+                    lines.add(new LineItem(Type.ADDED, newFileLines.get(0)));
+                    newFileLines.remove(0);
                 }
             }
-        } catch (IOException e){
-            e.getStackTrace();
+        }
+        if (oldFileLines.size() > 0) lines.add(new LineItem(Type.REMOVED, oldFileLines.get(0)));
+        else if (newFileLines.size() > 0) lines.add(new LineItem(Type.ADDED, newFileLines.get(0)));
+        for (LineItem i : lines) {
+            System.out.println(i.type + " " + i.line);
         }
     }
 
+    static List<String> readFileLines(String fileName) throws IOException {
+        BufferedReader fReader = new BufferedReader(new FileReader(fileName));
+        List<String> fileLines = new ArrayList<>();
+        String line;
+        while ((line = fReader.readLine()) != null) {
+            fileLines.add(line);
+        }
+        fReader.close();
+        return fileLines;
+    }
 
     public static enum Type {
         ADDED,        //добавлена новая строка
